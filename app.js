6db5d3896fa1a4004be828a605b7ad9a80a0cdc6
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-17 00:03:47 +0200

    js

---
+++ app.js
@@ -0,0 +1,20 @@
+$('.button').on('click', function(e) {
+    e.preventDefault();
+    $(this).addClass('active');
+  });
+  
+  $('.modal-wrapper').find('label').on('click', function() {
+    $('.button').removeClass('active');
+  });
+  
+  $('.input-text').focus(function() {
+    $(this).parents('.input-box').addClass('focus');
+  })
+  
+  $('.input-text').blur(function() {
+    if ($(this).val()) {
+      $(this).parents('.input-box').addClass('focus');
+    } else {
+      $(this).parents('.input-box').removeClass('focus');
+    }
+  });
